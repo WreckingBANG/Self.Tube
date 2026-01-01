8df6437a9e7d7020a31982a3f3e5b79231207454
WreckingBANG <wreckingbang@mailbox.org>
2026-01-01 11:02:09 +0100

    fix: replaced polling streams for video_player; refs #46

--- lib/widgets/media/video/adapters/vp-adapter.dart
+++ lib/widgets/media/video/adapters/vp-adapter.dart
@@ -1,20 +1,61 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import '../video_player_interface.dart';
 
 class VideoPlayerAdapter implements MediaPlayer {
   final VideoPlayerController _controller;
-  late Future<void> _initializeFuture;
+  late final Future<void> _initializeFuture;
+
+  final _positionController = StreamController<Duration>.broadcast();
+  final _playingController = StreamController<bool>.broadcast();
+
+  Duration? _lastPosition;
+  bool? _lastPlaying;
+
+  int _lastEmitMs = 0;
+  bool _disposed = false;
 
   VideoPlayerAdapter(String url, {Map<String, String>? headers})
       : _controller = VideoPlayerController.networkUrl(
           Uri.parse(url),
           httpHeaders: headers ?? const {},
         ) {
-    _initializeFuture = _controller.initialize().then((_) async {
-      await _controller.seekTo(const Duration(milliseconds: 1));
+    _initializeFuture = _controller.initialize().then((_) {
+      if (!_controller.value.isInitialized) return;
+
+      final value = _controller.value;
+
+      _lastPlaying = value.isPlaying;
+      _lastPosition = value.position;
+
+      _playingController.add(_lastPlaying!);
+      _positionController.add(_lastPosition!);
+
       _controller.play();
+      _controller.addListener(_onControllerUpdate);
     });
+  }
+
+  void _onControllerUpdate() {
+    if (_disposed) return;
+
+    final value = _controller.value;
+    if (!value.isInitialized) return;
+
+    if (value.isPlaying != _lastPlaying) {
+      _lastPlaying = value.isPlaying;
+      _playingController.add(value.isPlaying);
+    }
+
+    final nowMs = DateTime.now().millisecondsSinceEpoch;
+    if (nowMs - _lastEmitMs >= 150) {
+      if (value.position != _lastPosition) {
+        _lastPosition = value.position;
+        _lastEmitMs = nowMs;
+        _positionController.add(value.position);
+      }
+    }
   }
 
   @override
@@ -48,32 +89,29 @@ class VideoPlayerAdapter implements MediaPlayer {
   Future<void> pause() => _controller.pause();
 
   @override
-  Future<void> seek(Duration position) async {
-    if (position < Duration.zero) {
-      return _controller.seekTo(Duration.zero);
-    } else if (position > duration) {
-      return _controller.seekTo(duration);
-    } else {
-      return _controller.seekTo(position);
-    }
+  Future<void> seek(Duration position) {
+    final d = duration;
+    final clamped = position < Duration.zero
+        ? Duration.zero
+        : (position > d ? d : position);
+    return _controller.seekTo(clamped);
   }
 
   @override
-  Stream<Duration> get positionStream =>
-      Stream.periodic(const Duration(milliseconds: 500), (_) {
-        return _controller.value.position;
-      });
+  Stream<Duration> get positionStream => _positionController.stream;
 
   @override
-  Stream<bool> get playingStream =>
-      Stream.periodic(const Duration(milliseconds: 500), (_) {
-        return _controller.value.isPlaying;
-      });
+  Stream<bool> get playingStream => _playingController.stream;
 
   @override
   bool get isPlaying => _controller.value.isPlaying;
 
   @override
-  void dispose() => _controller.dispose();
+  void dispose() {
+    _disposed = true;
+    _controller.removeListener(_onControllerUpdate);
+    _controller.dispose();
+    _positionController.close();
+    _playingController.close();
+  }
 }
-
