e362980477a14bca970bf10391cd401c07abc524
WreckingBANG <wreckingbang@mailbox.org>
2026-03-28 16:00:50 +0100

    fix: app crash when launching app for first time

--- lib/features/onboarding/domain/user_session.dart
+++ lib/features/onboarding/domain/user_session.dart
@@ -1,3 +1,4 @@
+import 'package:Self.Tube/common/data/services/settings/settings_service.dart';
 import 'package:Self.Tube/features/onboarding/data/api/user_api.dart';
 import 'package:Self.Tube/features/onboarding/data/models/userinfo_model.dart';
 
@@ -7,9 +8,10 @@ class UserSession {
   static UserInfoModel? user;
 
   static Future<void> init() async {
-    user = await UserApi().fetchUserModel();
-
-    isPrivileged = user != null && (user!.isStaff || user!.isSuperUser);
+    if (SettingsService.doneSetup == true) {
+      user = await UserApi().fetchUserModel();
+      isPrivileged = user != null && (user!.isStaff || user!.isSuperUser);
+    }
   }
 }
 
