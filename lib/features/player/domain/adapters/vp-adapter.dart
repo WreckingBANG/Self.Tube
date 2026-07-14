9480a6d75a255951179b5f35402a3a5fd91451c0
WreckingBANG <wreckingbang@mailbox.org>
2026-07-14 18:51:08 +0200

    fix: reverted to video_player to textureView; refs: #97

--- lib/features/player/domain/adapters/vp-adapter.dart
+++ lib/features/player/domain/adapters/vp-adapter.dart
@@ -29,7 +29,6 @@ class VideoPlayerAdapter implements MediaPlayer {
       : _controller = VideoPlayerController.networkUrl(
           Uri.parse(url),
           httpHeaders: headers ?? const {},
-          viewType: VideoViewType.platformView
         ) {
     _initialized = _controller.initialize().then((_) {
       if (!_controller.value.isInitialized) return;
